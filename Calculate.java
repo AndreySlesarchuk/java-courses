7fd0d2a5c36b4444416507e848d5a18902d3258c
Andrey <andrey.slesarchuk@gmail.com>
2018-06-22 16:33:12 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,12 @@
+/**
+  Класс для вычисления суммы двух чисел. Числа берутся извходяшего массива.
+*/
 public class Calculate {
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("Sum : " + sum);
 	}
-	
 }
