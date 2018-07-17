2e0a5cf04144c96dd0c2ac4f894185b81c30bf43
Andrey <andrey.slesarchuk@gmail.com>
2018-07-17 23:40:02 +0300

    add old exaples

---
+++ Party.java
@@ -0,0 +1,20 @@
+
+//c:\Program Files\Java\jdk1.8.0_102\bin\
+
+import java.awt.*;
+import java.awt.event.*;
+public class Party {
+	
+	public static void main(String[] args){
+		buildInvite();
+	}
+	public void buildInvite() {
+		Frame f = new Frame();
+		Label l = new Label("Вечеринка у Андрея");
+		Button b = new Button("Ваша ставка");
+		Button c = new Button("Сбросить");
+		Panel p = new Panel();
+		p.add(l);
+		
+	} // Еще код
+}
